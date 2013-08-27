a9fed0e343d16eaef827fb3aa399543a1d54eaa6
Jacques Labuschagne <jlabusch@acm.org>
2013-08-28 00:26:35 +1200

    Removed custom model store; modify WRs instead.

--- server/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-STUNNEL_PID=/tmp/stunnel.pid
-KANBAN_PID=/tmp/wrms-kanban.pid
-
-(test -f $KANBAN_PID && kill $(cat $KANBAN_PID)) || echo "wrms-kanban not running"
-killall stunnel4
-
--- wrms-kanban.js
+++ wrms-kanban.js
@@ -1,28 +1,11 @@
 (function(){
-
-    var __log_fn_name_stack = [];
-
-    function __enter(name, silent){
-        __log_fn_name_stack.unshift(name);
-        if (!silent){
-            log.info('{enter}');
-        }
-    }
-
-    function __leave(silent){
-        __log_fn_name_stack.shift();
-        if (!silent){
-            log.info('{leave}');
-        }
-    }
-
     var log = {
         error: function(func, msg, except){
             var a = Array.prototype.slice.call(arguments, 0);
             if (a.length < 3 && __log_fn_name_stack.length){
                 a.unshift(__log_fn_name_stack[0]);
             }
-            console.error('wrms-kanban.js:' + a[0] + ' - ' + a[1] + (a[2] ? '[[' + a[2] + ']]' : ''));
+            console.error('wrms-kanban.js:' + a[0] + ' - ' + a[1] + (a[2] ? ' [[' + a[2] + ']]' : ''));
         },
         info: function(func, msg){
             var a = Array.prototype.slice.call(arguments, 0);
@@ -62,140 +45,98 @@
     }
 
 
-    var model_host = 'https://foo.wgtn.cat-it.co.nz:27601';
     var __model = {};
-    var __page_key = null;
+    var __parent_wr = null;
 
-    function store_model(m, callback){
-        log.info('store_model', 'Storing model for ' + __page_key + '...');
-        $.ajax({
-            type: 'POST',
-            url: model_host + '/put',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            data: JSON.stringify({key: __page_key, val: m})
-        })
-        .done(function(r){
-            log.info('store_model', 'done ' + r);
-            callback(null, r);
-        })
-        .fail(function(o, e){
-            log.error('store_model', 'failed', e);
-            callback(e);
-        });
-    }
+    var __wrms_status_map = {
+        'On Hold': 'H',
+        'Quote Approved': 'A',
+        'Quoted': 'Q',
+        'Testing': 'T',
+        'Finished': 'F',
+        'Allocated': 'L',
+        'In Progress': 'I',
+        'Cancelled': 'C',
+        'New request': 'N',
+        'Reviewed': 'R',
+        'New request': 'N',
+        'Reviewed': 'R',
+        'On Hold': 'H',
+        'Cancelled': 'C',
+        'In Progress': 'I',
+        'Allocated': 'L',
+        'Finished': 'F',
+        'Provide Feedback': 'K',
+        'Testing/Signoff': 'T',
+        'Quoted': 'Q',
+        'Quote Approved': 'A',
+        'Needs Documenting': 'D',
+        'Ready for Staging': 'S',
+        'Production Ready': 'P',
+        'Failed Testing': 'Z',
+        'Catalyst Testing': 'U',
+        'QA Approved': 'V',
+        'Pending QA': 'W',
+        'Need Info': 'B',
+        'Ongoing Maintenance': 'O',
+        'Development Complete': 'E',
+        'Blocked': 'X'
+    };
 
-    function get_stored_model(callback){
-        log.info('get_stored_model', 'Getting model for ' + __page_key + '...');
-        $.ajax({
-            type: 'GET',
-            url: model_host + '/get',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            data: {key: __page_key}
-        })
-        .done(function(r){
-            log.info('get_stored_model', 'done ' + r);
-            callback(null, r || {});
-        })
-        .fail(function(o, e){
-            log.error('get_stored_model', 'failed', e);
-            callback(e);
-        });
-    }
+    var __category_meta = {
+        backlog:    {name: 'Backlog',   statuses: ['New request', 'Quoted']},
+        this_week:  {name: 'This week', statuses: ['Allocated', 'Quote Approved']},
+        cat_dev:    {name: 'Dev',       statuses: ['In Progress', 'Ongoing Maintenance', 'Needs Documenting',
+                                                   'Provide Feedback', 'Reviewed', 'Development Complete',
+                                                   'Failed Testing']},
+        cat_test:   {name: 'Test',      statuses: ['Ready for Staging', 'Catalyst Testing', 'QA Approved']},
+        cat_blocked:{name: 'Blocked',   statuses: ['Need Info', 'On Hold', 'Blocked']},
+        client_uat: {name: 'UAT',       statuses: ['Pending QA', 'Testing/Signoff']},
+        done:       {name: 'Done',      statuses: ['Production Ready', 'Finished', 'Cancelled']}
+    };
 
-    function parse_child_relations(){
-        var result = [];
-        $('table.entry').each(function(){
-            try{
-                var table = $(this);
-                if (table.find('th.pcol:first').text() !== 'This W/R'){
-                    return;
-                }
-                table.find('tbody > tr').each(function(){
-                    var row = $(this);
-                    if (row.hasClass('row0') || row.hasClass('row1')){
-                        var line = {
-                            wr:     row.find('td.entry:nth-of-type(3) > a').text(),
-                            brief:  row.find('td.entry:nth-of-type(4) > a').text(),
-                            type:   row.find('td.entry:nth-of-type(2)').text(),
-                            status: row.find('td.entry:nth-of-type(5)').text()
-                        };
-                        if (line.type === 'Implemented in'){
-                            result.push(line);
-                        }
-                    }
-                });
-            }catch(ex){
-                log.error('parse_child_relations', 'exception', ex);
+    function category_from_status(s){
+        var result = null;
+        _.each(__category_meta, function(val, key){
+            if (result){
+                return;
+            }
+            if (_.contains(val.statuses, s)){
+                result = key;
             }
         });
         return result;
     }
 
-    function add_allocations(m){
-        _.each(m, function(val, key){
-            $.ajax({
-                type: 'GET',
-                url: model_host + '/enrich',
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json',
-                data: {key: __page_key, wr: key}
-            })
-            .done(function(r){
-                log.info('add_allocations', key + ': ' + JSON.stringify(r));
-                val.__kanban['users'] = r;
-                render_allocation(key, val);
-            })
-            .fail(function(o, e){
-                log.error('add_allocations', key + ' failed', e);
-                callback(e);
-            });
-        });
-        return m;
-    }
-
-    // TODO: combine with filter vv
-    var __category_meta = {
-        backlog:    {name: 'Backlog'},
-        this_week:  {name: 'This week'},
-        cat_dev:    {name: 'Dev'},
-        cat_test:   {name: 'Test'},
-        cat_blocked:{name: 'Blocked'},
-        client_uat: {name: 'UAT'},
-        done:       {name: 'Done'}
-    };
-
-    function update_model(rels, old_m){
-        var m = {};
-        var filter = [
-            {key: 'backlog',    re: /New request|Quoted/},
-            {key: 'this_week',  re: /Allocated|Quote Approved/},
-            {key: 'cat_dev',    re: /Ongoing|Failed Testing|Needs Documenting|Provide Feedback|In Progress|Reviewed/},
-            {key: 'cat_test',   re: /Development Complete|Catalyst Testing|Ready for Staging/},
-            {key: 'cat_blocked',re: /Need Info|On Hold|Blocked/},
-            {key: 'client_uat', re: /QA Approved|Pending QA/},
-            {key: 'done',       re: /Production Ready|Testing\/Signoff|Finished|Cancelled/}
-        ];
-        rels.forEach(function(r){
-            if (old_m[r.wr]){
-                r.__kanban = old_m[r.wr].__kanban;
-                m[r.wr] = r;
-                return;
+    function get_children(callback){
+        $.ajax({
+            type: 'GET',
+            url: '/api2/search?filtertable=wrsearch&format=json&columns=request_id%2Cdescription%2Cstatus%2Callocated_to&offset=0&limit=200&q=childof%3A' + __parent_wr
+        })
+        .done(function(r){
+            try{
+                var model = {};
+                r = JSON.parse(r);
+                r.response.body.forEach(function(item){
+                    var wr = item.request_id[0];
+                    model[wr] = {
+                        wr: wr,
+                        status: item.status[0],
+                        brief: item.description[0],
+                        cat: category_from_status(item.status[0]),
+                        users: item.allocated_to // possibly undefined
+                    };
+                });
+                callback(null, model);
+            }catch(ex){
+                log.error('get_children', 'failed for parent ' + __parent_wr, ex);
+                callback(ex);
             }
-            var seen = false;
-            filter.forEach(function(f){
-                if (seen){
-                    return;
-                }
-                if (r.status.match(f.re)){
-                    seen = true;
-                    r.__kanban = {cat: f.key};
-                    m[r.wr] = r;
-                }
-            });
+        })
+        .fail(function(o, e){
+            log.error('add_allocations', key + ' failed', e);
+            callback(e);
         });
-        return m;
     }
 
     function maybe_create_overlay_dom(){
@@ -210,38 +151,6 @@
                     $(d).append(mk('h1', [], function(h1){ $(h1).text($('td.entry').eq(1).text()); }));
                 }));
                 $(row).append(mk('div', ['col', 'span_1_of_6'], function(d){
-                    function unmark_modified(){
-                        $('#kanban-overlay li.modified').each(function(){
-                            $(this).removeClass('modified');
-                        });
-                    }
-                    $(d).append(mk('a', ['btn', 'save'], function(a){
-                        $(a).text('[Save]')
-                            .click(function(){
-                                store_model(__model, function(e, r){
-                                    if (e){
-                                        log.error('save-btn', 'failed to save model', e);
-                                    }else{
-                                        unmark_modified();
-                                        log.info('save-btn', 'saved model ' + r);
-                                    }
-                                });
-                            });
-                    }));
-                    $(d).append(mk('a', ['btn', 'reset'], function(a){
-                        $(a).text('[Reset]')
-                            .click(function(){
-                                store_model({}, function(e, r){
-                                    if (e){
-                                        log.error('save-btn', 'failed to save model', e);
-                                    }else{
-                                        unmark_modified();
-                                        log.info('save-btn', 'saved model ' + r);
-                                        kanban.show();
-                                    }
-                                });
-                            });
-                    }));
                     $(d).append(mk('a', ['btn', 'close'], function(a){
                         $(a).text('[Close]')
                             .click(function(){
@@ -251,8 +160,7 @@
                 }));
             }));
             function add_list(to, cat){
-                $(to)//.append(mk('h2', [], function(h2){ $(h2).text(__category_meta[cat].name); }))
-                     .append(mk('ul', ['wrl', 'kanban-' + cat], function(ul){
+                $(to).append(mk('ul', ['wrl', 'kanban-' + cat], function(ul){
                         $(ul).append(mk('li', ['heading'], function(li){
                             $(li).text(__category_meta[cat].name);
                         }));
@@ -277,31 +185,42 @@
             connectWith: '#kanban-overlay ul',
             revert: true,
             receive: function(evt, ui){
-                console.log(evt);
-                console.log(ui);
                 $(ui.item).addClass('modified');
                 var wr = $(ui.item).find('.wrno').text();
-                if (wr){
-                    var kb = __model[wr].__kanban;
-                    var new_cat = $(ui.item).parent().attr('class').match(/kanban-([_a-z]+)/);
-                    if (new_cat){
-                        kb.old_cat = kb.cat;
-                        kb.cat = new_cat[1];
-                        log.info('kanban ul:receive', wr + ' ' + kb.old_cat + ' -> ' + kb.cat);
-                    }else{
-                        log.error('kanban ul:receive', 'model not updated', 'Cannot determine parent class');
-                    }
+                var new_cat = $(ui.item).parent().attr('class').match(/kanban-([_a-z]+)/);
+                if (new_cat){
+                    new_cat = new_cat[1];
+                }else{
+                    log.error('kanban ul:receive', wr, 'Failed to determine parent class');
+                    return;
                 }
-                //TODO: update __model
+                var new_stat_long = __category_meta[new_cat].statuses[0];
+                var new_stat_short = __wrms_status_map[new_stat_long];
+                $.ajax({
+                    type: 'POST',
+                    url: '/api2/request_update',
+                    contentType: 'application/x-www-form-urlencoded',
+                    data: {
+                        request_id: wr,
+                        status: new_stat_short
+                    }
+                }).fail(function(o, e){
+                    log.error('kanban ul:receive', wr + ' failed to update status', e);
+                }).done(function(r){
+                    log.info('kanban ul:receive', 'updated ' + wr);
+                    $(ui.item).removeClass('modified');
+                    $(ui.item).find('span.status').text('[' + new_stat_long + ']');
+                    __model[wr].cat = new_cat;
+                    __model[wr].status = new_stat_long;
+                });
             }
         }).disableSelection();
     }
 
-    function lay_out_cards(m){
+    function render_model(m){
         $('#kanban-overlay li:not(.heading)').remove();
         _.each(m, function(val, key){
-            $('ul.kanban-' + val.__kanban.cat).append(mk('li', [], function(li){
-                //log.info('lay_out_cards', JSON.stringify(val));
+            $('ul.kanban-' + val.cat).append(mk('li', [], function(li){
                 $(li).html(
                     '<span class="wrno_pretty"><a href="https://wrms.catalyst.net.nz/wr.php?edit=1&request_id=' + val.wr + '">[#' + val.wr + ']</a></span>' +
                     '<span class="status">[' + val.status + ']</span>' +
@@ -311,20 +230,16 @@
             }));
             render_allocation(key, val);
         });
-    }
-
-    function render_model(m){
-        //log.info('render_model', JSON.stringify(m));
-        lay_out_cards(m);
+        return m;
     }
 
     function render_allocation(wr, data){
-        if (!data.__kanban || !data.__kanban.users || data.__kanban.users[0] === 'Nobody'){
+        if (!data.users || data.users[0] === 'Nobody'){
             return;
         }
         var li = $('span.wrno:contains(' + wr + ')').parent();
         $(li).find('span.alloc').remove();
-        data.__kanban.users.forEach(function(u){
+        data.users.forEach(function(u){
             $(li).append(
                 mk('span', ['alloc'], function(s){
                     $(s).text(u);
@@ -335,14 +250,16 @@
 
     var kanban = {
         show: function(){
-            var child_relations = parse_child_relations();
-            log.info('kanban.show', 'found ' + child_relations.length + ' "I" relations');
-            get_stored_model(function(err, model){
-                __model = add_allocations(update_model(child_relations, model));
-                render_model(__model);
-                $('#kanban-overlay').height($(document).height());
-                $('#kanban-overlay').show();
+            get_children(function(e, r){
+                if (e){
+                    // TODO
+                    $('#kanban-overlay').hide();
+                    return;
+                }
+                __model = render_model(r);
             });
+            $('#kanban-overlay').height($(document).height());
+            $('#kanban-overlay').show();
         },
         hide: function(){
             $('#kanban-overlay').hide();
@@ -351,7 +268,7 @@
 
     $(document).ready(function(){
         try{
-            __page_key = $('#tmnu > a:nth-of-type(1)').text();
+            __parent_wr = $('#tmnu > a:nth-of-type(1)').text().match(/(\d+)/)[1];
             maybe_create_overlay_dom();
             if (!$('#tmnu_kanban').length){
                 var tmnu = $('#tmnu');
@@ -370,7 +287,7 @@
                 }
             });
         }catch(ex){
-            console.log('Exception while adding Kanban menu entry [[' + ex + ']]');
+            log.error('wrms-kanban', 'Exception while adding Kanban menu entry', ex);
         }
     });
 })();
